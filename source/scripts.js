fed1736e11d3a23716013591d0c105c139a69c84
Nolan Kovacik <noltron000@gmail.com>
2019-12-05 15:23:47 -0800

    [add] promise for all!

--- source/scripts.js
+++ source/scripts.js
@@ -10,8 +10,6 @@ const title = document.getElementById('title')
 const description = document.getElementById('description')
 const form = document.getElementById('form')
 
-console.log(form)
-
 form.addEventListener('submit', async (e) => {
 	e.preventDefault()
 	const json = await getTaco()
@@ -24,7 +22,15 @@ const getTaco = async () => {
 	const res = await fetch(path)
 	const json = await res.json()
 
-	console.log(json)
+	const all = []
+	for (item in json) {
+		console.log(item)
+		all.push(fetch(item.url),then(res => res.text()))
+	}
+	
+	const values = await Promise.all(all)
+	console.log(values)
+ 
 
 	return json
 }
